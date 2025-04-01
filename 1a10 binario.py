cb088542c37a5e1d82476ded17087737a35e1f94
Manuel Macias <jmaciasdelara@gmail.com>
2025-04-01 08:13:59 -0600

    update.1a10 binario.py

--- 1a10 binario.py
+++
@@ -1,7 +0,0 @@
-print("decimal  binario  octal  hexadecimal")
-for i in range (1, 11, 1):
-   binario=bin(i)[2:]
-   octal=oct(i)[2:]
-   hexadecimal=hex(i)[2:]
-   print(i, binario, octal, hexadecimal)
-   
